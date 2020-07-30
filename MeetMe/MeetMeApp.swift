659e8eee541897d67cc0f4e5dbab6cb991a788c5
hajdinim <hajdinim@uni.coventry.ac.uk>
2020-07-30 12:59:12 +0100

    Initial Commit

---
+++ MeetMe/MeetMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetMeApp.swift
+//  MeetMe
+//
+//  Created by Tony Hajdini on 30/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct MeetMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
