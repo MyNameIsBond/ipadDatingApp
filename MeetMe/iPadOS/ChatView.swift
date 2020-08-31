ea13e386fadf1b44229e7b019740e4894c64ebad
hajdinim <hajdinim@uni.coventry.ac.uk>
2020-08-31 03:26:55 +0100

    messaging View

--- MeetMe/iPadOS/ChatView.swift
+++ MeetMe/iPadOS/ChatView.swift
@@ -26,7 +26,7 @@ struct ChatView: View {
                 }, label: {
                     Image(systemName: "photo").foregroundColor(Color.accentColor)
                 })
-                TextField("Message",text:$mess).background(BlurView(style: .regular)).padding(5).cornerRadius(9.0)
+                TextField("Message",text:$mess).padding(5).overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.gray, lineWidth: 1)).background(BlurView(style: .regular)).cornerRadius(20)
                 Button(action: {
                     print("photo")
                 }, label: {
