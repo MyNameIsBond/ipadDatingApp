cc47f5601d9e219b839e45b181819d226b5580f8
hajdinim <hajdinim@uni.coventry.ac.uk>
2020-09-01 16:43:16 +0100

    Action Sheet Done!

--- MeetMe/iPadOS/ChatView.swift
+++ MeetMe/iPadOS/ChatView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ChatView: View {
     var message = [messages]()
     @State private var mess: String = ""
+    @State private var showingActionSheet = false
     var body: some View {
             VStack {
                 ZStack(alignment: .bottom) {
@@ -25,10 +26,17 @@ struct ChatView: View {
                     }
                 HStack {
                     Button(action: {
-                        print("photo")
+                        self.showingActionSheet.toggle()
                     }, label: {
                         Image(systemName: "plus").foregroundColor(Color.accentColor)
-                    })
+                    }).actionSheet(isPresented: $showingActionSheet) {
+                        ActionSheet(title: Text(""), message: Text(""), buttons: [
+                            .default(Text("Gallery")) {  },
+                            .default(Text("Camera")) { },
+                            .default(Text("Blue")) { },
+                            .cancel()
+                        ])
+                    }
                     TextField("Message",text:$mess).padding(5).overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.gray, lineWidth: 2)).background(BlurView(style: .light)).cornerRadius(20)
                     Button(action: {
                         // To Photo Gallery
